5599ca4321ea2fad2470093c98388e537668814b
Lokesh kanna Bharathi <lokbhaarthy@gmail.com>
2017-02-24 00:56:33 +0800

    Added functionality to delete and edit icons

--- Probusiness-Template/js/wishlist.js
+++ Probusiness-Template/js/wishlist.js
@@ -11,4 +11,35 @@ function initWishListPage(){
 		$("#myWishPage").show();
 		$("#addWishPage").hide();
 	});
+	
+	$(".deleteline").on('click', function(event){
+		var parent = $(this).parent().parent().remove();
+	});
+	
+	$(".editline").on('click', function(event){
+		var parent = $(this).parent().parent();
+		parent.children('td').each(function () {
+			if(this.textContent != undefined && this.textContent != ""){
+				if(this.cellIndex == 0){
+					$("#itemName").val(this.textContent);
+				}
+				if(this.cellIndex == 1){
+					$('#category').val(this.textContent);
+					$('#category').selectpicker('refresh');
+				}
+				if(this.cellIndex == 2){
+					$('#exchangeItem').val(this.textContent);
+					$('#exchangeItem').selectpicker('refresh');
+				}
+				if(this.cellIndex == 3){
+					$('#datepicker').data({date: this.textContent}).datepicker('update').children("input").val(this.textContent);
+				}
+				if(this.cellIndex == 4){
+					$("#comment").val(this.textContent);
+				}
+			}
+		});
+		$("#myWishPage").hide();
+		$("#addWishPage").show();
+	});
 }
